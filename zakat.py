e2e4c5dbc8b451877d6d32571652b243a697c63f
filinodz <63097559+filinodz@users.noreply.github.com>
2024-04-09 18:37:32 +0100

    Add files via upload

--- zakat.py
+++ zakat.py
@@ -39,8 +39,8 @@ class ZakatElFitrCalculator(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('حاسبة زكاة الفطر')
-        self.setWindowIcon(QIcon('img/icon.png'))  # Remplacer 'icon.png' par le chemin de votre icône
-        self.setStyleSheet("background-color: #f0f0f0;")  # Couleur de fond de l'application
+        self.setWindowIcon(QIcon('img/icon.png'))  
+        self.setStyleSheet("background-color: #f0f0f0;")  
         self.initUI()
 
     def initUI(self):
@@ -48,25 +48,24 @@ class ZakatElFitrCalculator(QWidget):
 
         header_label = QLabel()
         header_pixmap = QPixmap("img/zakat_fitr.jpg")
-        header_pixmap = header_pixmap.scaledToWidth(400)  # Diminuer la taille de l'image
+        header_pixmap = header_pixmap.scaledToWidth(500)  
         header_label.setPixmap(header_pixmap)
         header_label.setAlignment(Qt.AlignCenter)
         vbox.addWidget(header_label)
 
         label_intro = QLabel("قد ثبت عن رسول الله ﷺ أنه فرض زكاة الفطر على المسلمين صاعاً من تمر أو صاعاً من شعير، وأمر بها أن تؤدى قبل خروج الناس إلى الصلاة، أعني صلاة العيد.")
-        label_intro.setStyleSheet("font-family: 'DIN Next LT Arabic'; font-size: 16pt; color: #333;")  # Utilisation de la police "DIN Next LT Arabic" avec une taille de police plus grande
+        label_intro.setStyleSheet("font-family: 'DIN Next LT Arabic'; font-size: 16pt; color: #333;")  
         label_intro.setAlignment(Qt.AlignCenter)
         vbox.addWidget(label_intro)
 
         label_personnes = QLabel("إختر عدد الأشخاص:")
-        label_personnes.setStyleSheet("font-family: 'DIN Next LT Arabic'; font-size: 14pt; color: #333;")  # Utilisation de la police "DIN Next LT Arabic" avec une taille de police plus grande
-        label_personnes.setAlignment(Qt.AlignCenter)
+        label_personnes.setStyleSheet("font-family: 'DIN Next LT Arabic'; font-size: 14pt; color: #333;") 
         vbox.addWidget(label_personnes)
 
         self.nombre_personnes_input = QLineEdit()
         self.nombre_personnes_input.setStyleSheet("border: 1px solid #ccc; border-radius: 5px; padding: 8px;")
         self.nombre_personnes_input.setAlignment(Qt.AlignCenter)
-        self.nombre_personnes_input.setFont(QFont("DIN Next LT Arabic", 14))  # Utilisation de la police "DIN Next LT Arabic" avec une taille de police plus grande
+        self.nombre_personnes_input.setFont(QFont("DIN Next LT Arabic", 14)) 
         vbox.addWidget(self.nombre_personnes_input)
 
         scroll_widget = QWidget()
@@ -78,15 +77,15 @@ class ZakatElFitrCalculator(QWidget):
 
             label_icon = QLabel()
             pixmap = QPixmap(f"img/{list(nourriture_zakat.keys()).index(nourriture) + 1}.jpg")
-            pixmap = pixmap.scaledToWidth(50)  # Taille fixe pour toutes les images
+            pixmap = pixmap.scaledToWidth(50) 
             label_icon.setPixmap(pixmap)
             hbox.addWidget(label_icon)
 
             checkbox = QCheckBox(nourriture, self)
             checkbox.setStyleSheet("QCheckBox { color: #333; }")
-            checkbox.setFont(QFont("DIN Next LT Arabic", 12))  # Utilisation de la police "DIN Next LT Arabic" avec une taille de police plus grande
+            checkbox.setFont(QFont("DIN Next LT Arabic", 12)) 
             hbox.addWidget(checkbox)
-            hbox.setAlignment(Qt.AlignCenter) # Alignement au centre
+            hbox.setAlignment(Qt.AlignCenter) 
             scroll_layout.addLayout(hbox)
 
             self.checkbox_vars.append(checkbox)
@@ -103,12 +102,12 @@ class ZakatElFitrCalculator(QWidget):
         self.calculate_button.setStyleSheet("QPushButton { color: #fff; background-color: #007bff; border: none; border-radius: 5px; padding: 10px; }"
                                              "QPushButton:hover { background-color: #0056b3; }")
         self.calculate_button.clicked.connect(self.calculer_zakat)
-        self.calculate_button.setFont(QFont("DIN Next LT Arabic", 12))  # Utilisation de la police "DIN Next LT Arabic" avec une taille de police plus grande
+        self.calculate_button.setFont(QFont("DIN Next LT Arabic", 12)) 
         vbox.addWidget(self.calculate_button)
 
         self.result_label = QLabel("")
         self.result_label.setAlignment(Qt.AlignCenter)
-        self.result_label.setStyleSheet("font-family: 'DIN Next LT Arabic'; font-size: 14pt; color: #333;")  # Utilisation de la police "DIN Next LT Arabic" avec une taille de police plus grande
+        self.result_label.setStyleSheet("font-family: 'DIN Next LT Arabic'; font-size: 14pt; color: #333;") 
         vbox.addWidget(self.result_label)
 
         self.setLayout(vbox)
@@ -144,8 +143,8 @@ class ZakatElFitrCalculator(QWidget):
 
             result_text = "زكاة الفطر لـ {} شخص :\n".format(nombre_personnes)
             for nourriture, quantite in total_quantite.items():
-                result_text += "{} كغ من {} بتكلفة {} دينار جزائري /كغ\n".format(int(quantite), nourriture, prix_nourriture[nourriture])
-            result_text += "\n  التكلفة الإجمالية: {} دينار جزائري".format(int(total_prix))
+                result_text += "{:.3f} كغ من {} بتكلفة {:.3f} دينار جزائري /كغ\n".format(quantite, nourriture, prix_nourriture[nourriture])
+            result_text += "\n  التكلفة الإجمالية: {:.3f} دينار جزائري".format(total_prix)
             self.result_label.setText(result_text)
 
         except ValueError:
